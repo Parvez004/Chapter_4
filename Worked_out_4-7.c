ede13f1897bb433b9f7d33edef6d8f760a98632e
Parvez004 <riadprvzz@gmail.com>
2023-02-11 12:58:35 +0600

    second

---
+++ Worked_out_4-7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    float sum=0;
+    for(i=1; i<=10; ++i)
+    {
+        sum = sum + 1/(float)i;
+        printf("\n%2d %6.4f", i, sum);
+    }
+    return 0;
+}
